af82f858b9a4a0320a3deaa3d6e48c60f56b6308
EbenAdex <110096723+EbenAdex@users.noreply.github.com>
2024-12-14 18:18:38 +0100

    Add files via upload

---
+++ gettogether.js
@@ -0,0 +1,34 @@
+// Function to generate the invitation message
+function inviteUserToClassGetTogether() {
+    const userName = document.getElementById("userName").value;
+    const date = "Friday, December 20th, 2024";
+    const time = "12:00 PM";
+    const location = "Ayomiposi house";
+    const activities = ["Games", "Music", "football for the boys", "Reconnecting"];
+    const foodAndDrinks = ["Alcohol", "sides", "Snacks", "NO-SOFT-DRINK"];
+  
+    const invitationMessage = `
+      Hi ${userName},
+  
+      You are cordially invited to Class-18 Get-Together!
+  
+      Details of the Hangout:
+      - Date:${date}
+      - Time: ${time}
+      - Location: ${location}
+      - Activities: ${activities.join(', ')}
+      - Food and Drinks: ${foodAndDrinks.join(', ')}
+  
+      Come and reconnect with old friends, make memories, and have a great time. We can't wait to see you there!
+  
+      Best regards,
+      Class Organizers
+    `;
+  
+    document.getElementById("invitationMessage").innerText = invitationMessage;
+  }
+  
+  
+  
+  console.log(exampleHTML);
+  
